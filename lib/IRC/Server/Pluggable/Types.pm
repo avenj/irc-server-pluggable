405eff10882f9303c1ae34b0ae538306011a3f73
avenj <avenj@cobaltirc.org>
2012-07-31 17:38:50 -0400

    (Seemingly) working IRC::Server::Pluggable::Types
    Still needs tests.
    
    	modified:   lib/IRC/Server/Pluggable/Types.pm

--- lib/IRC/Server/Pluggable/Types.pm
+++ lib/IRC/Server/Pluggable/Types.pm
@@ -3,9 +3,10 @@ package IRC::Server::Pluggable::Types;
 use strictures 1;
 
 use base 'Exporter';
-use MooX::Types::MooseLike::Base;
+use MooX::Types::MooseLike;
+use MooX::Types::MooseLike::Base qw/:all/;
 
-our @EXPORT_OK;
+our @EXPORT_OK = ();
 
 use Scalar::Util qw/blessed/;
 
@@ -15,12 +16,27 @@ my $type_definitions = [
     test => sub { blessed($_[0]) && $_[0]->isa('POE::Wheel') },
     message => sub { "$_[0] is not a POE::Wheel" },
   },
+  {
+    name => 'Filter',
+    test => sub { blessed($_[0]) && $_[0]->isa('POE::Filter') },
+    message => sub { "$_[0] is not a POE::Filter" },
+  },
 ];
 
 MooX::Types::MooseLike::register_types(
   $type_definitions, __PACKAGE__
 );
 
-our @EXPORT = \@EXPORT_OK;
+our @EXPORT = (
+  @EXPORT_OK,
+  qw/
+   Any Defined Undef Bool
+   Str Num Int
+   Ref ArrayRef HashRef CodeRef 
+   RegexpRef GlobRef
+   FileHandle Object
+   AHRef
+  /,
+);
 
 1;

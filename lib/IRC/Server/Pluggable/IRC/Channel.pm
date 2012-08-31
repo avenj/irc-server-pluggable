ce699d8e61d86859a212308cabcb80ef9fcdf100
avenj <avenj@cobaltirc.org>
2012-08-30 20:20:10 -0400

    IRC::Channel; add topic(), ts(), some user_* methods
    	modified:   lib/IRC/Server/Pluggable/IRC/Channel.pm

--- lib/IRC/Server/Pluggable/IRC/Channel.pm
+++ lib/IRC/Server/Pluggable/IRC/Channel.pm
@@ -25,7 +25,7 @@ has 'name' => (
 );
 
 has 'nicknames' => (
-  lazy => 1,
+  lazy    => 1,
   is      => 'ro',
   isa     => HashRef[ArrayRef],
   writer  => 'set_nicknames',
@@ -34,9 +34,10 @@ has 'nicknames' => (
 
 has 'modes' => (
   ##  Status modes are handled via nicknames hash and chg_status()
-  ##  Relies on ->prefix_map() and ->valid_channel_modes() from Protocol
-  ##  to find out what modes actually are/do, so this all has to be
-  ##  outside of these per-channel objects
+  ##  FIXME Relies on ->prefix_map() and ->valid_channel_modes() from
+  ##  Protocol to find out what modes actually are/do, so this all has to be
+  ##  outside of these per-channel objects or we need weak_refs back
+  ##  Probably belongs in a Role.
   lazy    => 1,
   is      => 'ro',
   isa     => HashRef,
@@ -44,19 +45,44 @@ has 'modes' => (
   default => sub { {} },
 );
 
+has 'topic' => (
+  ## Array of topic details
+  ##  [ string, setter, TS ]
+  lazy      => 1,
+  is        => 'ro',
+  isa       => Array,
+  writer    => 'set_topic',
+  predicate => 'has_topic',
+  clearer   => 'clear_topic',
+  default   => sub { [ ] },
+);
+
+has 'ts' => (
+  ## Channel's timestamp.
+  ## Most common Protocols can make use of this.
+  required => 1,
+  is       => 'ro',
+  isa      => Num,
+  writer   => 'set_ts',
+  clearer  => 'clear_ts',
+);
+
 
 has '_list_classes' => (
   ## Map list keys to classes
+  init_arg => 'list_classes',
   lazy    => 1,
   is      => 'ro',
   isa     => HashRef,
   writer  => '_set_list_classes',
   builder => '_build_list_classes',
+);
 
 sub _build_list_classes {
   my $base = "IRC::Server::Pluggable::IRC::Channel::List::";
   {
-      bans => $base . "Bans",
+      bans    => $base . "Bans",
+      invites => $base . "Invites",
   }
 }
 
@@ -67,10 +93,10 @@ has 'lists' => (
   is      => 'ro',
   isa     => HashRef,
   writer  => 'set_lists',
-  builder => 'build_lists',
+  builder => '_build_lists',
 );
 
-sub build_lists {
+sub _build_lists {
   ## Construct from _list_classes
   my ($self) = @_;
 
@@ -93,7 +119,8 @@ sub build_lists {
 ##  May reconsider this later ...
 
 
-## User manip
+### Users
+
 sub add_user {
   my ($self, $nickname, $data) = @_;
 
@@ -120,8 +147,92 @@ sub users_as_array {
   [ keys %{ $self->nicknames } ]
 }
 
+sub _param_isa_user_obj {
+  my ($self, $obj) = @_;
+  return unless is_Object($user)
+    and $user->isa('IRC::Server::Pluggable::IRC::User');
+  1
+}
 
-## Mode manip
+## FIXME it's possible all of this should move up a level
+## to Channels; we're probably going to need a weak_ref to
+## our Protocol instance, and it'd be more efficient to
+## keep just the one-per-Protocol in Channels.
+
+sub user_is_banned {
+  my ($self, $user) = @_;
+
+  confess "user_is_banned got $user, expected ::IRC::User"
+    unless $self->_param_isa_user_obj($user);
+
+  ## FIXME consult List:: objects
+  ##  Return true if matches_mask
+  ##  This means we need to know our casemap.
+}
+
+sub user_can_send {
+  my ($self, $user) = @_;
+
+  confess "user_can_send got $user, expected ::IRC::User"
+    unless $self->_param_isa_user_obj($user);
+
+  ## FIXME
+  ## Need some smart methods to check moderated, status modes, etc
+  ## Need to be able to flexibly allow users with status modes to
+  ## talk through moderated/banned
+  ## Need to handle external messages (cmode +n)
+  ## Needs to be easily overridable for subclass modes like +q, +R, +M
+  ## No-go if disallowed by check methods
+  ## Not sure if base Channel class should let opers override
+}
+
+sub user_can_join {
+  ## FIXME check user_is_banned, check +k / +l
+  ## (Subclasses can deal with exemptions, sslonly etc)
+}
+
+### Invites
+
+sub add_invite {
+  ## FIXME
+}
+
+sub del_invite {
+  ## FIXME
+}
+
+
+### Modes
+
+sub get_pub_or_secret_char {
+  my ($self) = @_;
+  ## FIXME
+  ## Return '=' if public
+  ##        '@' if secret
+  ##        '*' if private
+  ## see hybrid7/src/channel.c channel_pub_or_secret()
+}
+
+sub get_status_char {
+  ## FIXME return status char for User
+  ##  These are prioritized in Protocol; we may need refs
+  ##  back to Protocol.. not really sane.
+  ##  Possible the Protocol attribs for cmodes should just move?
+  ##  Keeping copies everywhere sucks too.
+  ##  Maybe a weak_ref back to the original attrib.
+  ### ...maybe a weak_ref back to Protocol.
+  my ($self, $user) = @_;
+  confess "get_status_char got $user, expected ::IRC::User"
+    unless $self->_param_isa_user_obj($user);
+}
+
+sub get_status_modes {
+  ## FIXME get array of current status modes for User
+  ## See notes in get_status_char.
+  my ($self, $user) = @_;
+  confess "get_status_modes got $user, expected ::IRC::User"
+    unless $self->_param_isa_user_obj($user);
+}
 
 sub chg_status {
   ## ->chg_status( $nickname, $mode_to_add, $excluded_modes )
@@ -155,13 +266,11 @@ sub chg_status {
 
 sub chg_modes {
   ## FIXME take a hash from mode_to_hash
+  ## Modes may have certain side-effects in a Protocol,
+  ## some of them may be handle-able from here.
+  ## We should at least be able to map a mode change to
+  ## method dispatch (via ->can('"hg_mode_$mode") or so)
+  ## or to a coderef invoked against $self perhaps
 }
 
-
-no warnings 'void';
-q{
- <LeoNerd> Hehe.. this does not bode well. I google searched for "MSWin32
-  socket non blocking connect", to read about how to do it. Got 1 404, 1
-  ancient article about 1990s UNIX, one about python, then the 4th
-  result is me, talking about how I don't know how to do it.
-};
+1;

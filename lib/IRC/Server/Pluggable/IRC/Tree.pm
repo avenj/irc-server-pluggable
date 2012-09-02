ef8bac0a74659bc560b5058636c73c5961d13e69
avenj <avenj@cobaltirc.org>
2012-09-02 01:56:54 -0400

    IRC::Tree; add path_to_server_indexes
    	modified:   lib/IRC/Server/Pluggable/IRC/Tree.pm

--- lib/IRC/Server/Pluggable/IRC/Tree.pm
+++ lib/IRC/Server/Pluggable/IRC/Tree.pm
@@ -86,46 +86,47 @@ sub path_to_server {
   return
 }
 
-sub path_to_server_array {
-  ## Like above, but return array of arrays describing the path
+sub path_to_server_indexes {
+  ## Like above, but return indexes into arrays describing the path
   my ($self, $server_name, $parent_ref) = @_;
 
   my @queue = ( PARENT => ($parent_ref || $self) );
   my %route;
 
   ## Breadth-first search.
-  while (my ($parent_name, $parent_ref) = splice @queue, 0, 2) {
+  my $parent_idx = 0;
+  PARENT: while (my ($parent_name, $parent_ref) = splice @queue, 0, 2) {
     ## Iterarate child nodes to find a route.
     ## The @queue is paths we haven't checked yet.
 
-    return [ ] if $parent_name eq $server_name;
+    return [ $parent_idx+1 ] if $parent_name eq $server_name;
 
     my @leaf_list = @$parent_ref;
-
-    while (my ($child_name, $child_ref) = splice @leaf_list, 0, 2) {
-      ## If we don't have a route for the name of this node,
-      ## set one up.
+    my $child_idx = 0;
+    CHILD: while (my ($child_name, $child_ref) = splice @leaf_list, 0, 2) {
       unless ( $route{$child_name} ) {
-        ## If our parent has a route, prepend it.
         $route{$child_name} =
-          [ @{ $route{$parent_name}||[] }, [ $child_name, $child_ref ] ];
+          [ @{ $route{$parent_name}||[] }, $child_idx+1 ];
 
-        ## If the child we're checking now is the one we're looking for,
-        ## return an arrayref of names.
         return \@{$route{$child_name}} if $child_name eq $server_name;
 
-        ## If we didn't hit on our target node yet, queue the child to
-        ## search.
-        push @queue, $child_name, $child_ref if @$child_ref;
+        push @queue, $child_name, $child_ref;
       }
-    }
 
-  }
+      $child_idx += 2;
+    }  ## CHILD
+
+    $parent_idx += 2;
+  }  ## PARENT
 
   return
 }
 
 sub child_node_for {
+  my ($self, $server_name, $parent_ref) = @_;
+  $parent_ref = $self unless defined $parent_ref;
+
+
 }
 
 sub add_server_to_hub {

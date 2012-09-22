fae3243f90fd9d8ed650349987049e7cff254881
avenj <avenj@cobaltirc.org>
2012-09-22 03:55:24 -0400

    Add Backend::EventSet objects
    	new file:   lib/IRC/Server/Pluggable/Backend/EventSet.pm
    	modified:   lib/IRC/Server/Pluggable/Protocol/Role/TS/Messages.pm

---
+++ lib/IRC/Server/Pluggable/Backend/EventSet.pm
@@ -0,0 +1,130 @@
+package IRC::Server::Pluggable::Backend::EventSet;
+
+use Carp;
+use strictures 1;
+
+use Scalar::Utils 'blessed';
+use Storable      'dclone';
+
+use IRC::Server::Pluggable::Backend::Event;
+
+use namespace::clean -except => 'meta';
+
+sub new {
+  my ($class, @events) = @_;
+  my $self = [];
+  bless $self, $class;
+
+  if (@events) {
+    $self->push(@events)
+  }
+
+  $self
+}
+
+sub _valid_ev {
+  my ($self, $ev) = @_;
+
+  EVENT: {
+    if (blessed $event
+      && $event->isa('IRC::Server::Pluggable::Backend::Event') ) {
+      last EVENT
+    }
+
+    if (ref $event eq 'HASH') {
+      $event = IRC::Server::Pluggable::Backend::Event->new(%$event);
+      last EVENT
+    }
+
+    confess "Expected Backend::Event or compatible HASH, got $event"
+  }  ## EVENT
+
+  $event
+}
+
+sub add { shift->push(@_) }
+
+sub by_index {
+  my ($self, $idx) = @_;
+
+  confess "by_index() expects an array index"
+    unless defined $idx;
+
+  $self->[$idx]
+}
+
+sub clone {
+  my ($self) = @_;
+  dclone($self)
+}
+
+sub list {
+  my ($self) = @_;
+  wantarray ? @$self : [ @$self ]
+}
+
+sub pop {
+  my ($self) = @_;
+  pop @$self
+}
+
+sub push {
+  my ($self, @events) = @_;
+  push @$self, map { $self->_valid_ev($_) } @events
+}
+
+sub shift {
+  my ($self) = @_;
+  shift @$self
+}
+
+sub unshift {
+  my ($self, @events) = @_;
+  unshift @$self, map { $self->_valid_ev($_) } @events
+}
+
+1;
+
+=head1 NAME
+
+IRC::Server::Pluggable::Backend::EventSet - Accumulate Backend::Events
+
+=head1 SYNOPSIS
+
+  my $evset = IRC::Server::Pluggable::Backend::EventSet->new(
+    {
+      prefix => $prefix,
+      target => $target,
+      params => [ @params ],
+    },
+
+  #    . . .
+
+  );
+
+  $evset->unshift( @prepend_events );
+
+  $evset->push( @more_events );
+
+  while (my $event = $evset->shift) {
+    my $prefix = $event->prefix;
+    #    . . .
+  }
+
+  my $last = $evset->pop;
+
+  my $second = $evset->by_index(1);
+
+=head1 DESCRIPTION
+
+An ARRAY-type object that takes (and validates) either
+L<IRC::Server::Pluggable::Backend::Event> object instances or a HASH that
+will be fed to L<IRC::Server::Pluggable::Backend::Event> ->new().
+
+
+FIXME
+
+
+=head1 AUTHOR
+
+Jon Portnoy <avenj@cobaltirc.org>
--- lib/IRC/Server/Pluggable/Protocol/Role/TS/Messages.pm
+++ lib/IRC/Server/Pluggable/Protocol/Role/TS/Messages.pm
@@ -3,7 +3,38 @@ package IRC::Server::Pluggable::Protocol::Role::TS::Messages;
 use Moo::Role;
 use strictures 1;
 
+use IRC::Server::Pluggable qw/
+  Backend::EventSet
+/;
+
+
 use namespace::clean -except => 'meta';
+
+
+requires qw/
+  config
+  send_to_routes
+/;
+
+sub _r_msgs_accumulate_targets {
+  ## FIXME
+  ##  Iterate target list
+  ##  Do hybrid-ish things
+  ##  Accumulate an EventSet
+}
+
+sub _r_msgs_can_send_to_chan {
+
+}
+
+sub _r_msgs_can_send_to_nick {
+
+}
+
+sub _r_msgs_can_send {
+
+}
+
 
 sub cmd_from_client_privmsg {}
 sub cmd_from_client_notice {}

e18650494e3f62b445d8a5fd8ebe20b8015b2fc1
avenj <avenj@cobaltirc.org>
2012-09-23 01:42:04 -0400

    Role::TS::Messages; most of the message-relay framework
    	modified:   lib/IRC/Server/Pluggable/Protocol/Role/TS/Messages.pm

--- lib/IRC/Server/Pluggable/Protocol/Role/TS/Messages.pm
+++ lib/IRC/Server/Pluggable/Protocol/Role/TS/Messages.pm
@@ -7,21 +7,95 @@ use IRC::Server::Pluggable qw/
   IRC::EventSet
 /;
 
+use Scalar::Util 'blessed';
 
 use namespace::clean -except => 'meta';
 
+### Basic message relay rules:
+## One-to-one:
+##  * User to local user
+##    - Relay locally
+##  * User to remote user
+##    - Relay to next-hop peer in this user's path
+##      ( ->route() )
+## One-to-many:
+##  * User to channel
+##   - Relay to own users
+##   - Accumulate route() IDs for users on channel,
+##     relay to each unique route()
+
+### Errors:
 
 requires qw/
   config
+  channels
+  channel_types
+  peers
+  users
   send_to_routes
 /;
 
-sub _r_msgs_accumulate_targets {
-  ## FIXME
-  ##  Iterate target list
-  ##  Do hybrid-ish things
-  ##  Accumulate an EventSet
+## FIXME also see _state_parse_msg_targets in PCSI
+##  + cmd_message / peer_message
+## some helpful pointers.
+
+sub _r_msgs_accumulate_targets_channel {
+  ## Accumulate a list of route IDs for a channel target
+  ## including routes to applicable local users
+  my ($self, $chan_name) = @_;
+
+  unless (blessed $chan) {
+    $chan = $self->channels->by_name($chan);
+  }
+
+  ## Should never be called for nonexistant chans:
+  confess "Expected an IRC::Channel but got $chan"
+    unless blessed $chan
+    and $chan->isa('IRC::Server:Pluggable::IRC::Channel');
+
+  my %routes;
+  for my $nick (@{ $chan_obj->nicknames_as_array }) {
+    my $user  = $self->users->by_name($nick);
+    $routes{ $user->route() } ||= 1;
+  }
+
+  my @routes = keys %routes;
+  wantarray ? @routes : \@routes
 }
+
+sub _r_msgs_accumulate_targets_servermask {
+  ## $$mask targets
+  my ($self, $mask) = @_;
+  my @peers = $self->peers->matching($mask);
+
+  my %routes;
+  for my $peer (@peers) {
+    $routes{ $peer->route() } ||= 1;
+  }
+
+  my @routes = keys %routes;
+  wantarray ? @routes : \@routes
+}
+
+sub _r_msgs_accumulate_targets_hostmask {
+  ## $#mask targets
+  my ($self, $mask) = @_;
+  my @users = $self->users->nuh_matching($mask);
+
+  my %routes;
+  for my $user (@users) {
+    $routes{ $user->route() } ||= 1;
+  }
+
+  my @routes = keys %routes;
+  wantarray ? @routes : \@routes
+}
+
+sub _r_msgs_accumulate_targets_statustype {
+  ## Status-prefixed targets, ie. @#channel-like targets
+
+}
+
 
 sub _r_msgs_can_send_to_nick {
 
@@ -33,8 +107,113 @@ sub _r_msgs_can_send {
   ##  Should we provide user_can_send_to_user ?
 }
 
+sub _r_msgs_parse_targets {
+  my ($self, @targets) = @_;
+  ## Borrowed from POE::Component::Server::IRC's target parser,
+  ## which is reasonably clever.
+  ## Turns a list of targets into a hash:
+  ##  $target => [ $type, @args ]
 
-sub cmd_from_client_privmsg {}
+  my @chan_prefixes = keys %{ $self->channel_types };
+  ## FIXME where do we set/get status prefixes...?
+
+  my %targets;
+
+  TARGET: for my $target (@targets) {
+
+    for my $prefix (@chan_prefixes) {
+      if ( substr($target, 0, 1) eq $prefix ) {
+        $targets{$target} = [ 'channel' ];
+        next TARGET
+      }
+    }
+
+    ## FIXME similar to above but check for valid status prefixes
+
+    ##   preceeding valid chan prefixes, preserve both as args
+    if ($target =~ /^\${2}(.+)$/) {
+      ## Server mask - $$mask
+      $targets{$target} = [ 'servermask', $1 ];
+      next TARGET
+    }
+
+    if ($target =~ /^\$\#(.+)$/) {
+      ## Host mask - $#mask
+      $targets{$target} = [ 'hostmask', $1 ];
+      next TARGET
+    }
+
+    if ($target =~ /@/) {
+      ## nick@server
+      my ($nick, $server) = split /@/, $target, 2;
+      my $host;
+      ($nick, $host) = split /%/, $nick, 2 if $nick =~ /%/;
+      $targets{$target} = [
+        'nick_fully_qualified',
+        $nick, $server, $host
+      ];
+      next TARGET
+    }
+
+    ## Fall through to nickname
+    $targets{$target} = [ 'nickname ];
+  } ## TARGET
+
+
+  \%targets
+}
+
+sub handle_message_relay {
+  ## handle_message_relay(
+  ##  prefix   => $nick,
+  ##  src_conn => $conn,
+  ##  string   => $string,
+  ##  targets  => [ @targets ],
+  ##  type     => $type,
+  ## FIXME do we need anything else ?
+  ## )
+  my ($self, %params) = @_;
+
+  for my $req (qw/prefix src_conn string targets type/) {
+    confess "missing argument '$req =>'";
+      unless defined $params{$req};
+  }
+
+  ## FIXME
+  ##  Parse targets
+  ##  Call handlers as-needed
+  ##  Accumulate EventSet
+  my $target_array = ref $params{targets} eq 'ARRAY' ?
+    $params{targets} : [ $params{targets} ];
+
+  my $targetset = $self->_r_msgs_parse_targets(@$target_array);
+  for my $target (keys %$targetset) {
+    my ($t_type, @t_params) = @{ $targetset->{$target} };
+    ## FIXME sanity checks, build EventSet if we hit errors
+    ##  - 481 if prefix nick not an oper and target is host/servermask
+    ##  - 413 if target is a mask and doesn't contain a .
+    ##  - 414 if target is a mask and doesn't look like a mask?
+    ##     pcsi uses !~ /\x2E.*[\x2A\x3F]+.*$/
+    ##  - 401 if channel / nick doesn't exist
+    ##  - 402 if nick_fully_qualified and peer doesn't exist
+    ##  - incr target count and 407 if we're over max targets
+    ## FIXME call appropriate methods to accumulate routes as-needed
+    ##  pcsi uses some funkyness in dispatching send_output ...
+    ## FIXME sort out full/nickname prefix situation
+  }
+}
+
+sub cmd_from_client_privmsg {
+  my ($self, $conn, $ev, $user) = @_;
+
+  $self->handle_message_relay(
+    type     => 'privmsg',
+    src_conn => $conn,
+    prefix   => $user->nick,
+    targets  => FIXME get params
+  );
+}
+
 sub cmd_from_client_notice {}
 
 sub cmd_from_peer_privmsg {}

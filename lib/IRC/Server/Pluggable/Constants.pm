b37c8887f1aa3b620055ea1c769d86af7b86695a
avenj <avenj@cobaltirc.org>
2012-09-30 21:48:14 -0400

    Constants; differentiate DISPATCH_* ints
    	modified:   lib/IRC/Server/Pluggable/Constants.pm

--- lib/IRC/Server/Pluggable/Constants.pm
+++ lib/IRC/Server/Pluggable/Constants.pm
@@ -4,14 +4,14 @@ our $VERSION = '0.000_01';
 use strictures 1;
 
 use constant {
-  DISPATCH_EATEN   => 1,
-  DISPATCH_CALLED  => 2,
-  DISPATCH_UNKNOWN => 3,
-
   EAT_NONE   => 1,
   EAT_CLIENT => 2,
   EAT_PLUGIN => 3,
   EAT_ALL    => 4,
+
+  DISPATCH_EATEN   => 5,
+  DISPATCH_CALLED  => 6,
+  DISPATCH_UNKNOWN => 7,
 };
 
 use base 'Exporter';
@@ -44,12 +44,6 @@ IRC::Server::Pluggable::Constants
 
 =head1 DESCRIPTION
 
-Exports constants used by L<IRC::Server::Pluggable::Protocol::Base>:
-
-  DISPATCH_EATEN   => 1
-  DISPATCH_CALLED  => 2
-  DISPATCH_UNKNOWN => 3
-
 Exports constants used by L<IRC::Server::Pluggable::Role::Pluggable> and
 L<IRC::Server::Pluggable::Role::Emitter>:
 
@@ -57,6 +51,12 @@ L<IRC::Server::Pluggable::Role::Emitter>:
   EAT_CLIENT => 2
   EAT_PLUGIN => 3
   EAT_ALL    => 4
+
+Exports constants used internally by L<IRC::Server::Pluggable::Protocol::Base>:
+
+  DISPATCH_EATEN   => 5
+  DISPATCH_CALLED  => 6
+  DISPATCH_UNKNOWN => 7
 
 =head1 AUTHOR
 

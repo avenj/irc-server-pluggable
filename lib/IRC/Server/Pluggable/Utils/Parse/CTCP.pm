ff501947904f31fa48356ca6544f88b17a00e23a
Jon Portnoy <avenj@cobaltirc.org>
2012-12-22 00:04:36 -0500

    Parse::CTCP cleanups, POD
    	modified:   lib/IRC/Server/Pluggable/Utils/Parse/CTCP.pm

--- lib/IRC/Server/Pluggable/Utils/Parse/CTCP.pm
+++ lib/IRC/Server/Pluggable/Utils/Parse/CTCP.pm
@@ -10,6 +10,9 @@ our @EXPORT = qw/
   ctcp_unquote
 /;
 
+use IRC::Server::Pluggable 'IRC::Event';
+use Scalar::Util 'blessed';
+
 my %quote = (
   "\012" => 'n',
   "\015" => 'r',
@@ -70,10 +73,102 @@ sub ctcp_unquote {
 }
 
 sub ctcp_extract {
-  ## FIXME
-  ##  ctcp_unquote and extract first CTCP
-  ##   return Event?
+  my ($input) = @_;
+
+  unless (blessed $input) {
+    $input = ref $input ? ev(%$input) : ev(raw_line => $input);
+  }
+
+  my $type = uc($input->command) eq 'PRIVMSG' ? 'ctcp' : 'ctcpreply' ;
+  my $line = $input->params->[1];
+  my $unquoted = ctcp_unquote($line);
+  return unless $unquoted;
+
+  my ($name, $params);
+  CTCP: for my $str ($unquoted->{ctcp}->[0]) {
+    ($name, $params) = $str =~ /^(\w+)(?: +(.*))?/;
+    last CTCP unless $name;
+    $name = lc $name;
+    if ($name eq 'dcc') {
+      ## Does no extra work to parse DCC
+      ## ... but see POE::Filter::IRC::Compat for that
+      my ($dcc_type, $dcc_params) = $params =~ /^(\w+) +(.+)/;
+      last CTCP unless $dcc_type;
+      return ev(
+        command => 'dcc_request_'.lc($dcc_type),
+        params  => [
+          $input->prefix,
+          $dcc_params
+        ],
+        raw_line => $input->raw_line,
+      )
+    } else {
+      return ev(
+        command => $type .'_'. $name,
+        params  => [
+          $input->prefix,
+          $input->params->[0],
+          ( defined $params ? $params : '' ),
+        ],
+        raw_line => $input->raw_line,
+      )
+    }
+  }
+  return
 }
 
 
 1;
+
+=pod
+
+=head1 NAME
+
+IRC::Server::Pluggable::Utils::Parse::CTCP - Parse incoming or outgoing CTCP
+
+=head1 SYNOPSIS
+
+FIXME
+
+=head1 DESCRIPTION
+
+Utility functions useful for quoting/unquoting/extracting CTCP.
+
+=head2 ctcp_extract
+
+Takes input (in the form of an L<IRC::Server::Pluggable::IRC::Event> instance,
+a hash such as that produced by L<IRC::Server::Pluggable::IRC::Filter>, or a
+raw line) and attempts to extract a valid CTCP request or reply.
+
+Returns an L<IRC::Server::Pluggable::IRC::Event> whose C<command> carries an
+appropriate prefix (one of B<ctcp>, B<ctcpreply>, or B<dcc_request>) prepended
+to the CTCP command:
+
+  $ev->command eq 'ctcp_version'      ## CTCP VERSION
+    $ev->params would be sender, target(s), any additional params
+  $ev->command eq 'ctcpreply_version' ## reply to CTCP VERSION
+    $ev->params would be sender, target(s), contents of reply
+  $ev->command eq 'dcc_request_send'  ## DCC SEND
+    $ev->params would be sender and DCC parameters
+
+Returns empty list if no valid CTCP was found.
+
+=head2 ctcp_quote
+
+CTCP quote a raw line.
+
+=head2 ctcp_unquote
+
+Deparses a raw line possibly containing CTCP.
+
+Returns a hash with two keys, B<ctcp> and B<text>, whose values are 
+ARRAYs containing the CTCP and text portions of a CTCP-quoted message.
+
+=head1 AUTHOR
+
+Code borrowed from L<POE::Filter::IRC::Compat>, copyright BinGOs, fimm et al
+
+Licensed under the same terms as Perl.
+
+=cut
+

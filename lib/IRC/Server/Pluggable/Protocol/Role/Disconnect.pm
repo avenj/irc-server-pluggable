bf93e333a8e5b4eac804dd9f94dbee8929e00383
Jon Portnoy <avenj@cobaltirc.org>
2013-01-04 02:27:59 -0500

    Protocol::Role::Disconnect sanity
    	modified:   lib/IRC/Server/Pluggable/Protocol/Role/Disconnect.pm

--- lib/IRC/Server/Pluggable/Protocol/Role/Disconnect.pm
+++ lib/IRC/Server/Pluggable/Protocol/Role/Disconnect.pm
@@ -15,7 +15,6 @@ use Carp;
 use Moo::Role;
 use strictures 1;
 
-
 use Scalar::Util 'blessed';
 
 ## Types returned by _r_disconnect_get_target_type($target)
@@ -30,11 +29,17 @@ use constant {
   UNKNOWN_CONN    => 7,
 };
 
+use IRC::Server::Pluggable qw/
+  IRC::Event
+/;
+
 
 use namespace::clean;
 
 with 'IRC::Server::Pluggable::Role::Interface::IRCd';
 
+
+### Handlers.
 sub cmd_from_unknown_quit {
   my ($self, $conn) = @_;
   $self->disconnect( $conn,
@@ -63,6 +68,222 @@ sub cmd_from_client_quit {
 }
 
 
+### Internal.
+
+sub _r_disconnect_with_error {
+  my ($self, $target_type, $conn, $msg) = @_;
+
+  ## Send ERROR and drop the Connect
+  ## These only go to locals, so we only take a ::Connect.
+
+  $self->send_to_routes(
+    {
+      command => 'ERROR',
+      params  => [ "Closing Link: ". $conn->peeraddr . " ($msg)" ],
+    },
+    $conn
+  );
+
+  $self->_r_disconnect_from_backend($conn);
+
+  ## FIXME call cleanups?
+}
+
+sub _r_disconnect_from_backend {
+  my ($self, $conn, $msg) = @_;
+
+  ## Backend handles these after a socket flush.
+  $conn->is_disconnecting($msg || "Client disconnect");
+}
+
+
+## Users.
+
+sub _r_disconnect_user_cleanup {
+  ## FIXME
+  ## call channel cleanups for user if we ->can()
+  ## (cleanup method should live in Channels role)
+  ## remove from ->users if this is a local user
+  ## remove from ->peers->by_name() for the user's peer if
+  ##  this is a remote user
+}
+
+sub _r_disconnect_user_quit {
+  my ($self, $target_type, $user, $msg) = @_;
+
+  if ($target_type == REMOTE_USER) {
+    ## A remote user quit has to be a KILL.
+    return $self->_r_disconnect_user_kill($target_type, $user, $msg);
+  }
+
+  ## FIXME
+}
+
+sub _r_disconnect_user_kill {
+  ## Low-level KILL send method
+  ## FIXME higher-level method to construct pretty kill messages
+  my ($self, $target_type, $user, $msg) = @_;
+  if ($target_type == LOCAL_USER) {
+    ## Local user.
+    ## FIXME call for cleanup
+    ## FIXME methods to flexibly send from/to TS6 IDs or non-TS names?
+    ##  Probably ought have an ->id attrib we can call instead of ->name
+    ##  non-TS items can just redirect to ->name
+    my @local_peers = $self->peers->list_local_peers;
+    for my $peer (@local_peers) {
+      if ($peer->type eq 'TS' && $peer->type_version == 6) {
+        $self->send_to_routes(
+          ev(
+            prefix  => 
+              ( $self->config->sid // $self->config->server_name ),
+            command => 'kill',
+            ## FIXME should this be ID or UID?
+            params  => [ $user->id, $msg ],
+          ),
+          $peer->route
+        ),
+        ## Prefix with SID, target UID.
+      } else {
+        ## Not a TS6 peer.
+        $self->send_to_routes(
+          ev(
+            prefix  => $self->config->server_name,
+            command => 'kill',
+            ## FIXME could just use ->id here also
+            ## and kill the code dupe
+            params  => [ $user->nick, $msg ],
+          ),
+          $peer->route
+        );
+      }
+
+    }
+  } elsif ($target_type == REMOTE_USER) {
+    ## Remote user
+    ##  FIXME
+  } else {
+    confess "No idea how to handle target_type $target_type"
+  }
+}
+
+
+## Peers.
+
+sub _r_disconnect_peer_cleanup {
+  ## FIXME
+  ## call channel cleanup routines for users lost
+  ## (should live in Channels role)
+  ## remove from ->peers
+}
+
+sub _r_disconnect_peer_squit {
+  my ($self, $target_type, $peer, $msg) = @_;
+  ## FIXME set is_disconnecting
+  ##  relay squit
+  ##  call channel cleanups for users
+  ##  ...
+}
+
+
+### Frontend.
+
+sub disconnect {
+  my ($self, $target, %params) = @_;
+
+  ## ->disconnect( $target_obj,
+  ##  type   => $type,  ## 'error', 'quit', 'kill'
+  ##  msg    => $disconnect_string,
+  ## )
+
+  confess "Expected a target object"
+    unless defined $target and blessed $target;
+
+  my $message     = $params{msg} // $params{message} // '';
+  my $action_type = uc($params{type} // 'QUIT' );
+
+  my ($target_type, $route_id)
+    = $self->_r_disconnect_get_target_type( $target );
+
+  for ($action_type) {
+    when ("ERROR") {
+      my $conn;
+
+      if ($target_type == LOCAL_USER || $target_type == LOCAL_PEER) {
+        ## Try to retrieve Backend::Connect for a User or Peer:
+        $conn = $target->conn;
+      } else {
+        ## ... elsewise we should've been passed a Backend::Connect
+        ## (*_CONN types)
+        $conn = $target;
+      }
+
+      ## We can't disconnect remotes with an ERROR; someone is an idiot:
+      confess "error type disconnect called but no Backend::Connect"
+        unless blessed $conn
+        and $conn->isa('IRC::Server::Pluggable::Backend::Connect');
+
+      ## Send ERROR and call for a backend disconnect:
+      $self->_r_disconnect_with_error( $target_type, $conn,
+        $message || "Unknown error from ".caller()
+      );
+    }
+
+    when ("KILL") {
+      confess "kill type disconnect called but target is not an IRC::User"
+        unless $target_type == LOCAL_USER or $target_type == REMOTE_USER;
+      $self->_r_disconnect_user_kill( $target_type, $target, $message );      
+    }
+
+    when ("QUIT") {
+      ## FIXME
+      ##  - could be:
+      ##    LOCAL_USER_CONN/LOCAL_PEER_CONN, convert to User/Peer objs
+      ##     and change to appropriate type
+      ##    LOCAL_USER  (discon, relay quit)
+      ##    LOCAL_PEER  (discon, relay squit, quits?)
+      ##    REMOTE_USER (send kill?)
+      ##    REMOTE_PEER (send squit)
+      QUIT: {
+
+        if ($target_type == LOCAL_USER_CONN) {
+          my $user = $self->users->by_id( $target->wheel_id ) || return;
+          $self->_r_disconnect_user_quit( $target_type, $user, $message );
+          last QUIT
+        }
+
+        if ($target_type == LOCAL_PEER_CONN) {
+          my $peer = $self->peers->by_id( $target->wheel_id ) || return;
+          $self->_r_disconnect_peer_squit( $target_type, $peer, $message );
+          last QUIT
+        }
+
+        if ($target_type == LOCAL_USER || $target_type == REMOTE_USER) {
+          $self->_r_disconnect_user_quit( $target_type, $target, $message );
+          last QUIT
+        }
+
+        if ($target_type == LOCAL_PEER || $target_type == REMOTE_PEER) {
+          $self->_r_disconnect_peer_squit( $target_type, $target, $message );
+          last QUIT
+        }
+
+      } # QUIT
+
+      when ("KILL") {
+
+      }
+
+    }
+
+  }
+
+  ## FIXME emit disconnected event ?
+
+  1
+}
+
+
+### Util.
 sub _r_disconnect_get_target_type {
   my ($self, $target) = @_;
 
@@ -114,171 +335,6 @@ sub _r_disconnect_get_target_type {
   } # TARGET
 
   wantarray ? ($target_type, $route_id) : $target_type
-}
-
-sub _r_disconnect_with_error {
-  my ($self, $conn, $msg) = @_;
-
-  ## Send ERROR and drop the Connect
-  ## These only go to locals.
-  ## disconnect() calls needed cleanup routines.
-
-  $self->send_to_routes(
-    {
-      command => 'ERROR',
-      params  => [ "Closing Link: ". $conn->peeraddr . " ($msg)" ],
-    },
-    $conn
-  );
-
-  $self->_r_disconnect_from_backend($conn);
-}
-
-sub _r_disconnect_from_backend {
-  my ($self, $conn, $msg) = @_;
-
-  ## Backend handles these after a socket flush.
-  $conn->is_disconnecting($msg || "Client disconnect");
-}
-
-sub _r_disconnect_user_cleanup {
-  ## FIXME
-  ## call channel cleanups for user if we ->can()
-  ## (cleanup method should live in Channels role)
-  ## remove from ->users if this is a local user
-  ## remove from ->peers->by_name() for the user's peer if
-  ##  this is a remote user
-}
-
-sub _r_disconnect_peer_cleanup {
-  ## FIXME
-  ## call channel cleanup routines for users lost
-  ## (should live in Channels role)
-  ## remove from ->peers
-}
-
-## FIXME methods for:
-##  - relay local user quit to all peers
-##  - relay remote user quit to all peers except src
-##  - relay squit
-
-sub _r_disconnect_quit_local_user {
-  ##  FIXME
-  ##  call _r_disconnect_with_error
-  ##  relay quit
-  ##  call channel cleanups
-}
-
-sub _r_disconnect_quit_local_peer {
-  ## FIXME set is_disconnecting
-  ##  relay squit
-  ##  call channel cleanups for users
-  ##  ...
-}
-
-sub _r_disconnect_quit_remote_user {
-  ## FIXME clear User object, cleanup channels, relay QUIT
-}
-
-sub _r_disconnect_quit_remote_peer {
-  ## FIXME relay squit, ...
-}
-
-sub disconnect {
-  my ($self, $target, %params) = @_;
-
-  ## ->disconnect( $target_obj,
-  ##  type   => $type,  ## 'error', 'quit'
-  ##  msg    => $disconnect_string,
-  ## )
-
-  confess "Expected a target object"
-    unless defined $target and blessed $target;
-
-  my $message     = $params{msg} // $params{message} // '';
-  my $action_type = uc($params{type} // 'QUIT' );
-
-  my ($target_type, $route_id)
-    = $self->_r_disconnect_get_target_type( $target );
-
-  ## FIXME 'kill' type instead of a remote user discon..?
-  for ($action_type) {
-    when ("ERROR") {
-      my $conn;
-
-      if ($target_type == LOCAL_USER || $target_type == LOCAL_PEER) {
-        ## Try to retrieve Backend::Connect for a User or Peer:
-        $conn = $target->conn;
-      } else {
-        ## ... elsewise we should've been passed a Backend::Connect
-        ## (*_CONN types)
-        $conn = $target;
-      }
-
-      ## We can't disconnect remotes with an ERROR; someone is an idiot:
-      confess "error-type disconnect called but no Backend::Connect"
-        unless blessed $conn
-        and $conn->isa('IRC::Server::Pluggable::Backend::Connect');
-
-      ## Send ERROR and call for a backend disconnect:
-      $self->_r_disconnect_with_error( $conn,
-        $message || "Unknown error from ".caller()
-      );
-    }
-
-    when ("QUIT") {
-      ## FIXME
-      ##  - could be:
-      ##    LOCAL_USER_CONN/LOCAL_PEER_CONN, convert to User/Peer objs
-      ##     and change to appropriate type
-      ##    LOCAL_USER  (discon, relay quit)
-      ##    LOCAL_PEER  (discon, relay squit, quits?)
-      ##    REMOTE_USER (send kill?)
-      ##    REMOTE_PEER (send squit)
-      QUIT: {
-
-        if ($target_type == LOCAL_USER_CONN) {
-          my $user = $self->users->by_id( $target->wheel_id ) || return;
-          $self->_r_disconnect_quit_local_user( $user, $message );
-          last QUIT
-        }
-
-        if ($target_type == LOCAL_PEER_CONN) {
-          my $peer = $self->peers->by_id( $target->wheel_id ) || return;
-          $self->_r_disconnect_quit_local_peer( $peer, $message );
-          last QUIT
-        }
-
-        if ($target_type == LOCAL_USER) {
-          $self->_r_disconnect_quit_local_user( $target, $message );
-          last QUIT
-        }
-
-        if ($target_type == LOCAL_PEER) {
-          $self->_r_disconnect_quit_local_peer( $target, $message );
-          last QUIT
-        }
-
-        if ($target_type == REMOTE_PEER) {
-          $self->_r_disconnect_quit_remote_peer( $target, $message );
-          last QUIT
-        }
-
-        if ($target_type == REMOTE_USER) {
-          $self->_r_disconnect_quit_remote_user( $target, $message );
-          last QUIT
-        }
-
-      } # QUIT
-
-    }
-
-  }
-
-  ## FIXME emit disconnected event ?
-
-  ## FIXME cleanup routines as-necessary, depending on what this is
-
 }
 
 1;
